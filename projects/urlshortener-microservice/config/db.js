2f0eea3b33a7e6467f58db1a04a2119c14ded6f7
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-10-14 22:37:05 -0500

    fixed bugs

--- projects/urlshortener-microservice/config/db.js
+++ projects/urlshortener-microservice/config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose')
 
 const connectDB = async (dbName) => {
-  if (mongoose.connection.readyState === 1) {
-    // Already connected
-    return mongoose.connection
-  }
 
-  const uri = process.env.MONGO_CONNECTION_STRING
-  if (!uri) throw new Error('❌ MONGO_CONNECTION_STRING not found in .env')
+  const url = process.env.MONGO_CONNECTION_STRING
+  if (!url) throw new Error('❌ MONGO_CONNECTION_STRING not found in .env')
 
   try {
-    await mongoose.connect(uri, {
+    await mongoose.connect(url, {
       dbName,
     })
     console.log('✅ Connected to MongoDB Atlas')
