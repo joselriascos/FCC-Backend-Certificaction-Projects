304026d96aeed462d16342e59ad48c66f6fd7d1f
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-10-19 21:13:05 -0500

    fixed bugs

--- projects/file-metadata-microservice/routes/fileRoutes.js
+++ projects/file-metadata-microservice/routes/fileRoutes.js
@@ -2,15 +2,13 @@ import express from 'express'
 import path from 'path'
 import upload from '../config/multerConfig.js'
 import { uploadFile } from '../controllers/fileController.js'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 const fileRouter = express.Router()
 
+const __dirname = process.cwd()
+
 fileRouter.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
 fileRouter.post('/upload', upload.single('upfile'), uploadFile)
