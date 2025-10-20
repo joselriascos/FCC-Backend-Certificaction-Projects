595aecfc0ecf8a24804a302ae70130e7cde83337
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-10-19 22:51:00 -0500

    fix

--- projects/file-metadata-microservice/functions/index.js
+++ projects/file-metadata-microservice/functions/index.js
@@ -3,10 +3,15 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import serverless from 'serverless-http'
 import fileRouter from '../routes/fileRoutes.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 dotenv.config()
 
 const app = express()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const port = process.env.PORT || 0
 
@@ -15,6 +20,9 @@ app.use(cors())
 app.use(express.json())
 
 // Routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+})
 app.use('/', fileRouter)
 
 // Serverless
--- projects/file-metadata-microservice/routes/fileRoutes.js
+++ projects/file-metadata-microservice/routes/fileRoutes.js
@@ -1,17 +1,8 @@
 import express from 'express'
-import path from 'path'
 import upload from '../config/multerConfig.js'
 import { uploadFile } from '../controllers/fileController.js'
-import { fileURLToPath } from 'url'
 
 const fileRouter = express.Router()
-
-const __filename = fileURLToPath(import.meta.url)
-
-fileRouter.get('/', (req, res) => {
-  res.sendFile(path.join(__filename, '..', '..', 'views', 'index.html'))
-  console.log(fileURLToPath(import.meta.url))
-})
 
 fileRouter.post('/upload', upload.single('upfile'), uploadFile)
 
