a09b9750e0e915724d0b5150a9219ec568d10e42
jose luis riascos cubillos <joselriascos@unicauca.edu.co>
2025-10-19 20:44:15 -0500

    fixed bugs

--- projects/file-metadata-microservice/functions/index.js
+++ projects/file-metadata-microservice/functions/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import serverless from 'serverless-http'
 import fileRouter from '../routes/fileRoutes.js'
 
 dotenv.config()
